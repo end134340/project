711673124842782ccb5704447fc275724609edd0
end134340 <end921@naver.com>
2025-06-19 11:40:23 +0900

    repository init

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+require('dotenv').config({path : './database/dbConfig.env'});
+const express = require("express");
+const app = express();
+const port = 3000;
+
+console.log(process.env.DB_NAME);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+app.get("/hello", (req, res) => {
+  console.log(req.url);
+  res.send("Hello World!");
+});
+
+
+app.get("/api/board", (req, res) => {
+  res.send({ title: "노드 api 서버 update!!!!" });
+});
+
+// vue.js build 이후
+
+//public폴더 아래의 정적 파일들 서버에 인식시킴.
+const path = require('path');
+const publicPath = path.join(__dirname, 'public');
+app.use(express.static(publicPath));
+
+//sendfile=> 절대 경로를 이용해 접근했을 때 파일을 전송. 파일을 읽어들여 그걸 res에 전송할 수 있도록 처리.
+app.get("/", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "./public", "index.html"));
+});
+
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "./public", "index.html"));
+});
---
+++ frontapp/vite.config.js
@@ -0,0 +1,38 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'node:path';
+
+const backServer = `http://localhost:3000`;
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+   // 개발용 임시 서버
+   server: {
+    // Vue.js 실행 시 적용 PORT 변경
+    port : 8099,
+     // CORS(Cross Origin Resource Sharing) => proxy setting
+    proxy: {
+      '^/api': {
+        target: backServer,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  build : {
+	//vite를 기반으로 배포를 할 때 추가적인 설정을 하는 것. 원래는 frontapp안에 빌드 결과물을 저장하는데 outDir를 해서 이 경로에 만드는?거 
+	//자동으로 그 위치에 결과물 저장.
+   outDir :  '../backend/public'
+  }
+})
